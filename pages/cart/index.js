bb307a1d8ac5c04c434b25bb29c0e95b60e6cf3b
= <=>
2020-05-14 17:18:15 +0800

    20200514

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,4 +1,5 @@
 /*
+
 1 获取用户收货地址
   1.1 绑定点击事件
   1.2 调用小程序内置的api获取用户的收货地址
@@ -25,13 +26,33 @@
   1 onShow 获取缓存中的购物车数组
   2 根据购物车中的商品数据 所有商品都被选中 checked=true 全选就都被选中
 5 总价格和总数量
-  1 都需要商品被选中 我们才拿它来计算
+  1 商品被选中后 我们才拿它来计算
   2 获取购物车数组
   3 遍历
   4 商品被选中
   5 总价格+=商品的单价*商品的数量
   5 总数量+=商品的数量
   6 把计算后的价格和数量 设置回data中即可
+6 商品的选取功能
+  1 绑定change事件
+  2 获取被修改的商品对象
+  3 商品对象的选中状态 取反
+  4 重新填充回data中和缓存中
+  5 重新计算全选 总价 总数量
+7 全选和反选
+  1 全选复选框绑定事件 change
+  2 获取data中的全选变量allChecked
+  3 直接取反allChecked=!allChecked
+  4 遍历购物车数组 让里面 商品 选中状态跟随 allChecked 改变而改变
+  5 把购物车数组和选中状态设置回data中，把购物车保存到缓存中
+8 商品数量的编辑
+  1 "+""-"按钮 绑定同一个点击事件 区分的关键 自定义属性
+    1 "+" "+1"  
+    2 "-" "-1"
+  2 传递被点击的商品id goods_id
+  3 获取data中的购物数组 来获取需要被修改的商品对象
+  4 直接修改商品对象的数组 num
+  5 把cart数组 重新设置回 缓存中 和data中 this.setCart
 
 */
 import { getSetting, chooseAddress, openSetting } from "../../utils/asyncWX.js"
@@ -50,28 +71,10 @@ Page({
     const address = wx.getStorageSync("address");
     // 1 获取缓存中的购物车数据
     const cart = wx.getStorageSync("cart") || [];
-    // 1 计算全选
-    // every数组方法会遍历 会接收一个回调函数，每次回调函数都返回true 那么erery方法返回值为true
-    // 只要有一次回调函数返回false 那么不再循环执行，直接返回false
-    // 空数组调用erery方法 返回值就是true
-    const allChecked = (cart.length != 0) ? cart.every(v => v.checked) : false;
-    // 1 总价格总数量
-    let totalPrice = 0;
-    let totalNum = 0;
-    cart.forEach(v => {
-      if (v.checked) {
-        totalPrice += v.num * v.goods_price;
-        totalNum += v.num;
-      }
-    });
-    // 2 给data赋值
-    this.setData({
-      address,
-      cart,
-      allChecked,
-      total
-    });
+    this.setData({ address });
+    this.setCart(cart);
   },
+  // 点击收货地址
   async handleChooseAddress() {
     console.log('干一行 行一行 一行行 行行行');
     try {
@@ -91,5 +94,60 @@ Page({
     catch (error) {
       console.log(error)
     }
+  },
+  // 处理商品的选中
+  handleItemChange: function (e) {
+    // 1 获取被修改的商品对象
+    const goods_id = e.currentTarget.dataset.id
+    // 2 获取购物车数组
+    let { cart } = this.data;
+    // 3 找到被修改的对象
+    let index = cart.findIndex(v => v.goods_id === goods_id);
+    // 4 选中状态取反
+    cart[index].checked = !cart[index].checked;
+    // 5 6 把购物车数据设定到data和缓存中
+    this.setCart(cart);
+  },
+  // 设置购物车状态同时 重新计算 底部工具栏 的数据：全选 总价 数量
+  setCart(cart) {
+    let allChecked = true;
+    let totalPrice = 0;
+    let totalNum = 0;
+    cart.forEach(v => {
+      if (v.checked) {
+        totalPrice += v.num * v.goods_price;
+        totalNum += v.num;
+      } else {
+        allChecked = false;
+      }
+    });
+    this.setData({
+      cart, totalPrice, totalNum, allChecked
+    })
+    wx.setStorageSync("cart", cart);
+  },
+  // 商品全选功能
+  handleItemAllCheck() {
+    // 1 获取data中的数据
+    let { cart, allChecked } = this.data;
+    // 2 allChecked取反
+    allChecked = !allChecked;
+    // 3 循环修改cart数组中的商品选中状态
+    cart.forEach(v => v.checked = allChecked);
+    // 4 把修改后的值 填充到data或缓存中
+    this.setCart(cart);
+
+  },
+  // 商品数量的编辑功能
+  handleItemNumEdit(e) {
+    // 1 获取传递过来的参数
+    const { operation, id } = e.currentTarget.dataset;
+    // 2 获取购物车数组
+    let { cart } = this.data;
+    // 3 找到需要修改的商品的索引
+    const index = cart.findIndex(v => v.goods_id === id);
+    // 4 进行修改数量
+    cart[index].num += parseInt(operation);
+    this.setCart(cart);
   }
 });
