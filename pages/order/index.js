8f135d0a92f4c44025a257f16a9323d3b95fede2
= <=>
2020-05-18 16:47:10 +0800

    20200518

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,66 +1,70 @@
-// pages/order/index.js
+/*
+1 页面打开的时候 onShow
+  0 onShow不同于onLoad 无法在形参上接收url中的参数
+  0.5 判断缓存中有没有token 
+    1 没有直接跳转到授权页面
+    2 有则继续执行
+  1 获取url上的参数
+  2 根据type去发送请求获取订单数据
+  3 然后渲染页面
+2 点击不同的标题 重新发送请求来获取和渲染数据
+*/ 
+
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime.js';
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        value: '全部',
+        isActive: true
+      },
+      {
+        id: 1,
+        value: '待付款',
+        isActive: false
+      },
+      {
+        id: 2,
+        value: '待发货',
+        isActive: false
+      },
+      {
+        id: 3,
+        value: '退货/退款',
+        isActive: false
+      }
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow(options){
+    const token=wx.getStorageSync("token");
+    if(!token){
+      wx.navigateTo({
+        url: '/pages/auth/index'
+      });
+      return;
+    }
+    
+    // 1 获取当前小程序的页面栈-数组 长度最大值是10
+    let pages =  getCurrentPages();
+    // 2 数组中索引最大的页面就是当前页面
+    let currentPage=pages[pages.length-1];
+    const {type}=currentPage.options;
+    this.getOrders(type);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  async getOrders(type){
+    const res=await request({url:"/my/orders/all",data:{type}});
+    console.log(res);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleTabsItemChange: function (e) {
+    // 获取被点击的标题索引
+    const { index } = e.detail;
+    // 2 修改源头数组
+    let { tabs } = this.data; // 这里的this指向当前页面对象
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    // 3 赋值到data中
+    this.setData({ tabs });
   }
 })
