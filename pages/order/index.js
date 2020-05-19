8bd31fca7ac09d460eacb536adc2677822c822d4
= <=>
2020-05-19 17:11:45 +0800

    20200519

--- pages/order/index.js
+++ pages/order/index.js
@@ -5,6 +5,7 @@
     1 没有直接跳转到授权页面
     2 有则继续执行
   1 获取url上的参数
+  2 根据typ来决定页面标题的数组元素 那个被激活选中
   2 根据type去发送请求获取订单数据
   3 然后渲染页面
 2 点击不同的标题 重新发送请求来获取和渲染数据
@@ -39,32 +40,46 @@ Page({
     ]
   },
   onShow(options){
-    const token=wx.getStorageSync("token");
-    if(!token){
-      wx.navigateTo({
-        url: '/pages/auth/index'
-      });
-      return;
-    }
+    // const token=wx.getStorageSync("token");
+    // if(!token){
+    //   wx.navigateTo({
+    //     url: '/pages/auth/index'
+    //   });
+    //   return;
+    // }
     
     // 1 获取当前小程序的页面栈-数组 长度最大值是10
     let pages =  getCurrentPages();
     // 2 数组中索引最大的页面就是当前页面
     let currentPage=pages[pages.length-1];
+    // 获取url上的type参数
     const {type}=currentPage.options;
+    // 激活选中页面标题
+    this.changeTitleByIndex(type-1);
     this.getOrders(type);
   },
   async getOrders(type){
-    const res=await request({url:"/my/orders/all",data:{type}});
-    console.log(res);
+    const res=await request({url:"/my/orders/all",data:{type}}) || {};
+    this.setData({
+      orders:res.orders.map(v=>({
+        ...v,
+        create_time_cn:(new Date(v.create_time*1000).toLocaleString())
+      }))
+    });
   },
-  handleTabsItemChange: function (e) {
-    // 获取被点击的标题索引
-    const { index } = e.detail;
+
+  changeTitleByIndex(index){ 
     // 2 修改源头数组
     let { tabs } = this.data; // 这里的this指向当前页面对象
     tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
     // 3 赋值到data中
     this.setData({ tabs });
+  },
+  handleTabsItemChange: function (e) {
+    // 1 获取被点击的标题索引
+    const { index } = e.detail;
+    changeTitleByIndex(index);
+    // 2 重新发送请求 type=index+1
+    this.getOrders(index+1);
   }
 })
