a7e292bd675c3963b9826e65e390245a0ef6eb75
= <=>
2020-05-21 17:01:31 +0800

    20200521

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,64 @@
-// pages/search/index.js
+/*
+1 输入框绑定 值改变事件 input事件
+  1 获取到输入框的值
+  2 合法性判断
+  3 检验通过 把输入框的值 发送到后台
+  4 返回的数据打印到页面上
+2 防抖防止抖动 定时器 节流
+  0 防抖一般输入框中防止重复输入 重复发送请求
+  1 节流 一般用作页面上拉 
+  2 定义一个全局的定时器
+*/
+
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime.js';
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goods: [],
+    // 取消的按钮是否显示
+    isFocus: false,
+    // 输入框中的值
+    inputValue:""
   },
+  TimeId: -1,
+  handleInput(e) {
+    // 1 获取输入框的值
+    const { value } = e.detail;
+    // 2 先检验合法性
+    if (!value.trim()) {
+      // 输入的值不合法
+      this.setData({
+        goods: [],
+        isFocus: false
+      });
+      return;
+    }
+    // 3 准备发送请求获取数据
+    this.setData({
+      isFocus: true
+    })
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    clearTimeout(this.TimeId);
+    this.TimeId = setTimeout(() => {
+      this.qsearch(value);
+    }, 1000);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 发送请求获取搜索建议 数据
+  async qsearch(query) {
+    const res = await request({ url: "/goods/qsearch", data: { query } });
+    let goods = res.data.message;
+    this.setData({
+      goods
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击取消按钮
+  handleCancel() {
+    this.setData({
+      inputValue:"",
+      isFocus:false,
+      goods:[]
+    });
   }
 })
--- pages/user/index.js
+++ pages/user/index.js
@@ -1,10 +1,13 @@
 // pages/user/index.js
 Page({
   data: {
-    userInfo:{}
+    userInfo:{},
+    // 被收藏的商品的数量
+    collectNum:0
   },
   onShow(){
     const userInfo=wx.getStorageSync("userInfo");
-    this.setData({userInfo});
+    const collect=wx.getStorageSync("collect")||[];
+    this.setData({userInfo,collectNum:collect.length});
   }
 })
